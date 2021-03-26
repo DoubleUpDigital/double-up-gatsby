049987627cc0fc8ec57fa44f836cc5331772799e
Justin Radomski <justin@doubleupdigital.com>
2021-03-26 14:04:18 -0400

    Removed manually placed index template

--- src/components/hero/Homepage/Homepage.js
+++ src/components/hero/Homepage/Homepage.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import parse from "html-react-parser"
+import Lottie from "lottie-react";
+import heroAnimation from "/content/assets/hero.json";
 
 const Homepage = data => {
   return (
 		<section id="Homepage">
+			<Lottie animationData={heroAnimation} />
 		  <h1>{data.heading}</h1>
 		</section>
   )
