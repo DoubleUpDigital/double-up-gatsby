3701f04274426d3b1ff7d9d0f382e2864859370e
Justin Radomski <justin@doubleupdigital.com>
2021-03-27 21:43:22 -0400

    Trying out the new GatsbyImage on the light hero blob

--- src/components/hero/Light/Light.js
+++ src/components/hero/Light/Light.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { StaticQuery, graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 import * as styles from "./light.module.scss"
 
 const Light = data => {
   return (
 		<section className={styles.hero}>
 			<div className="header-spacer"></div>
+      <StaticQuery
+        query={graphql`
+          {
+            file(name: {eq: "blob-top-right"}) {
+              id
+              childImageSharp {
+                gatsbyImageData(layout: FULL_WIDTH, placeholder: TRACED_SVG)
+              }
+            }
+          }
+        `}
+        render={data => <GatsbyImage className={styles.hero__blob} image={data.file.childImageSharp.gatsbyImageData} alt="" />}
+      ></StaticQuery>
+      
 			<div className={styles.hero__content}>
 				<div className="container">
 					<span className={`${styles.hero__tag} tag`}>{data.title}</span>
