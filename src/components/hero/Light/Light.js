38e7a607c33b5ddfa61bb39fe5a150e6c47f7427
Justin Radomski <justin@doubleupdigital.com>
2021-03-27 22:01:11 -0400

    Testing StaticImage instead of GatsbyImage

--- src/components/hero/Light/Light.js
+++ src/components/hero/Light/Light.js
@@ -1,26 +1,40 @@
 import React from 'react'
 import { StaticQuery, graphql } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image"
+import { StaticImage, GatsbyImage } from "gatsby-plugin-image"
 import * as styles from "./light.module.scss"
 
 const Light = data => {
   return (
 		<section className={styles.hero}>
 			<div className="header-spacer"></div>
-      <StaticQuery
-        query={graphql`
-          {
-            file(name: {eq: "blob-top-right"}) {
-              id
-              childImageSharp {
-                gatsbyImageData(layout: FULL_WIDTH, placeholder: TRACED_SVG)
-              }
-            }
-          }
-        `}
-        render={data => <GatsbyImage className={styles.hero__blob} image={data.file.childImageSharp.gatsbyImageData} alt="" />}
-      ></StaticQuery>
-      
+      {
+        //<StaticQuery
+      //   query={graphql`
+      //     {
+      //       file(name: {eq: "blob-top-right"}) {
+      //         id
+      //         childImageSharp {
+      //           gatsbyImageData(layout: FULL_WIDTH, placeholder: TRACED_SVG)
+      //         }
+      //       }
+      //     }
+      //   `}
+      //   render={data => (
+      //     <GatsbyImage 
+      //       className={styles.hero__blob} 
+      //       image={data.file.childImageSharp.gatsbyImageData} 
+      //       alt="" 
+      //       style={{position: "absolute"}} />
+      //     )
+      //   }
+      // ></StaticQuery>
+    }
+      <StaticImage 
+        className={styles.hero__blob} 
+        src="../../../ui/blob-top-right.png"
+        placeholder="tracedSVG"
+        alt="" 
+        style={{position: "absolute"}} />
 			<div className={styles.hero__content}>
 				<div className="container">
 					<span className={`${styles.hero__tag} tag`}>{data.title}</span>
