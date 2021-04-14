28fdf35303e4c9bbd6030546d6bde7e3013a5d35
Justin Radomski <justin@doubleupdigital.com>
2021-04-14 17:15:48 -0400

    Swapped out SVG rendered for canvas

--- src/components/hero/Homepage/Homepage.js
+++ src/components/hero/Homepage/Homepage.js
@@ -9,7 +9,7 @@ const Homepage = data => {
     <section className={styles.hero}>
       <div className={styles.hero__background}>
         <div className={styles.hero__background_front}></div>
-        <Lottie animationData={heroAnimation} />
+        <Lottie className={styles.hero__background_animation} animationData={heroAnimation} renderer="canvas" />
       </div>
       
       <div className={styles.hero__content}>
