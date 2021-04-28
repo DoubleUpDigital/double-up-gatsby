7cfcc2fff44c1f596fb3b0a63b670ff81334e3e3
Justin Radomski <justin@justinradomski.me>
2021-04-28 11:02:06 -0400

    Added slick to project slider

--- src/components/page/ProjectSlider/ProjectSlider.js
+++ src/components/page/ProjectSlider/ProjectSlider.js
@@ -3,8 +3,12 @@ import { GatsbyImage } from "gatsby-plugin-image"
 import * as styles from "./projectSlider.module.scss"
 import { Link } from "gatsby"
 
-import { faLongArrowRight } from '@fortawesome/pro-regular-svg-icons'
+import { faLongArrowRight, faLongArrowLeft } from '@fortawesome/pro-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import "slick-carousel/slick/slick.css";
+
+import Slider from "react-slick";
 
 const List = data => {
   return (
@@ -15,31 +19,52 @@ const List = data => {
 				<div className={`${styles.projectSlider__content} margin-fix`} dangerouslySetInnerHTML={{ __html:data.content }}></div>
 			</div>
             <div className="container">
-                {data.projects.map((project,i) => (
-                    <div className="project-slide" key={'project_' + i}>
-                        <GatsbyImage className={`${styles.projectSlider__slide_image}`} image={project.projectDetails.screenshots.primaryDesktop.localFile.childImageSharp.gatsbyImageData} />
-                        <div className={styles.projectSlider__slide_info}>
-                            <div className={styles.projectSlider__slide_info_block}>
-                                <span className="tag tag--yellow">Project</span>
-                                <h3>{project.title}</h3>
-                            </div>
-                            <div className={styles.projectSlider__slide_info_block}>
-                                <span className="tag tag--red">What we did</span>
-                                <h3>{project.projectDetails.whatWeDid}</h3>
-                            </div>
-                            <div className={styles.projectSlider__slide_info_cta}>
-                                <Link to={project.uri} className="button button--inverted">
-        							<span className="button__text">
-        								View Project
-        							</span>
-        							<span className="button__orb">
-        								<FontAwesomeIcon icon={faLongArrowRight} />
-        							</span>
-        						</Link>
+                <Slider
+                    dots={false}
+                    slidesToShow={1}
+                    slidesToScroll={1}
+                    autoplay={true}
+                    autoplaySpeed={3000}
+                    fade={true}
+                    className={styles.projectSlider__slider}
+                    arrows={true}
+                    nextArrow={(
+                        <button className="slick-arrow slick-next">
+                            <FontAwesomeIcon className="slick-arrow-icon" icon={faLongArrowRight} />
+                        </button>
+                    )}
+                    prevArrow={(
+                        <button className="slick-arrow slick-prev">
+                            <FontAwesomeIcon className="slick-arrow-icon" icon={faLongArrowLeft} />
+                        </button>
+                    )}
+                >
+                    {data.projects.map((project,i) => (
+                        <div className="project-slide" key={'project_' + i}>
+                            <GatsbyImage className={`${styles.projectSlider__slide_image}`} image={project.projectDetails.screenshots.primaryDesktop.localFile.childImageSharp.gatsbyImageData} />
+                            <div className={styles.projectSlider__slide_info}>
+                                <div className={styles.projectSlider__slide_info_block}>
+                                    <span className="tag tag--yellow">Project</span>
+                                    <h3>{project.title}</h3>
+                                </div>
+                                <div className={styles.projectSlider__slide_info_block}>
+                                    <span className="tag tag--red">What we did</span>
+                                    <h3>{project.projectDetails.whatWeDid}</h3>
+                                </div>
+                                <div className={styles.projectSlider__slide_info_cta}>
+                                    <Link to={project.uri} className="button button--inverted">
+            							<span className="button__text">
+            								View Project
+            							</span>
+            							<span className="button__orb">
+            								<FontAwesomeIcon icon={faLongArrowRight} />
+            							</span>
+            						</Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </Slider>
             </div>
 		</section>
   )
