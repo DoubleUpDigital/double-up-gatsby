ace641613ab7ca4615a21c59dd2c125d0fdeba78
Avery Williams <averyhwilliams@yahoo.com>
2021-04-29 13:15:02 -0400

    Styled DataSlider component.

--- src/components/page/DataSlider/DataSlider.js
+++ src/components/page/DataSlider/DataSlider.js
@@ -2,25 +2,50 @@ import React from 'react'
 import * as styles from "./dataSlider.module.scss"
 import { Link } from "gatsby"
 
+import "slick-carousel/slick/slick.css";
+
+import Slider from "react-slick";
+
+import { faLongArrowRight, faLongArrowLeft } from '@fortawesome/pro-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLongArrowRight } from '@fortawesome/pro-regular-svg-icons'
 
 const DataSlider = data => {
   return (
 		<section className={styles.dataSlider}>
 
-            <div className="container container--small">
+            <div className="container container--slider">
                 <div className={`${styles.dataSlider__slides}`}>
-					{data.dataSlides.map((slide,i) => (
-                        <div className={`${styles.dataSlider__slide}`}>
-                            <div className={`${styles.dataSlider__slide_content}`} dangerouslySetInnerHTML={{ __html:slide.slideContent }}></div>
-                            <div className={`${styles.dataSlider__slide_data}`}>
-                                <div className={`${styles.dataSlider__slide_dataPrefix}`}>{slide.slideDataPrefix}</div>
-                                <div className={`${styles.dataSlider__slide_dataNumber}`}>{slide.slideData}</div>
-                                <div className={`${styles.dataSlider__slide_dataSuffix}`}>{slide.slideDataSuffix}</div>
+                    <Slider
+                        dots={false}
+                        slidesToShow={1}
+                        slidesToScroll={1}
+                        autoplay={true}
+                        autoplaySpeed={5000}
+                        fade={true}
+                        className={styles.dataSlider__slider}
+                        arrows={true}
+                        nextArrow={(
+                            <button className="slick-arrow slick-next">
+                                <FontAwesomeIcon className="slick-arrow-icon" icon={faLongArrowRight} />
+                            </button>
+                        )}
+                        prevArrow={(
+                            <button className="slick-arrow slick-prev">
+                                <FontAwesomeIcon className="slick-arrow-icon" icon={faLongArrowLeft} />
+                            </button>
+                        )}
+                    >
+    					{data.dataSlides.map((slide,i) => (
+                            <div className={`${styles.dataSlider__slide}`}>
+                                <div className={`${styles.dataSlider__slide_content}`} dangerouslySetInnerHTML={{ __html:slide.slideContent }}></div>
+                                <div className={`${styles.dataSlider__slide_data}`}>
+                                    <div className={`${styles.dataSlider__slide_dataPrefix}`}>{slide.slideDataPrefix}</div>
+                                    <div className={`${styles.dataSlider__slide_dataNumber}`}>{slide.slideData}</div>
+                                    <div className={`${styles.dataSlider__slide_dataSuffix}`}>{slide.slideDataSuffix}</div>
+                                </div>
                             </div>
-                        </div>
-					))}
+    					))}
+                    </Slider>
 				</div>
             </div>
 
