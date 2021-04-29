b81eeadb5f0873e32b6c630e9eb4b6608895816d
Avery Williams <averyhwilliams@yahoo.com>
2021-04-29 15:53:53 -0400

    Made TeamGrid component.

---
+++ src/components/page/TeamGrid/TeamGrid.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import * as styles from "./teamGrid.module.scss"
+import { Link } from "gatsby"
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLongArrowRight } from '@fortawesome/pro-regular-svg-icons'
+
+const TeamGrid = data => {
+  return (
+		<section className={`${styles.teamGrid}`}>
+
+
+		</section>
+  )
+}
+
+export default TeamGrid
