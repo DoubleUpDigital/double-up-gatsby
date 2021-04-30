d18d35965a817a3b4019d6bb7f2e459a605fa048
Avery Williams <averyhwilliams@yahoo.com>
2021-04-30 12:41:01 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,6 +79,7 @@ module.exports = {
      */
     `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
