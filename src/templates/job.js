9373c09e3225eaa2de1f5434e607efe0d9d0f9e5
Justin Radomski <justin@doubleupdigital.com>
2021-05-03 17:08:21 -0400

    Added jobs post type

---
+++ src/templates/job.js
@@ -0,0 +1,90 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Image from "gatsby-image"
+import parse from "html-react-parser"
+
+// We're using Gutenberg so we need the block styles
+import "@wordpress/block-library/build-style/style.css"
+import "@wordpress/block-library/build-style/theme.css"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const JobTemplate = ({ data: { post } }) => {
+  const featuredImage = {
+    fluid: post.featuredImage?.node?.localFile?.childImageSharp?.fluid,
+    alt: post.featuredImage?.node?.alt || ``,
+  }
+
+  return (
+    <Layout>
+      <SEO title={post.seo.title} description={post.seo.metaDesc} />
+
+      <article
+        className="blog-post"
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <header>
+          <h1 itemProp="headline">{parse(post.title)}</h1>
+
+          <p>{post.date}</p>
+
+          {/* if we have a featured image for this post let's display it */}
+          {featuredImage?.fluid && (
+            <Image
+              fluid={featuredImage.fluid}
+              alt={featuredImage.alt}
+              style={{ marginBottom: 50 }}
+            />
+          )}
+        </header>
+
+        {!!post.content && (
+          <section itemProp="articleBody">{parse(post.content)}</section>
+        )}
+
+        <hr />
+
+        <footer>
+          <Bio />
+        </footer>
+      </article>
+
+    </Layout>
+  )
+}
+
+export default JobTemplate
+
+export const pageQuery = graphql`
+  query JobById(
+    # these variables are passed in via createPage.pageContext in gatsby-node.js
+    $id: String!
+  ) {
+    # selecting the current post by id
+    post: wpJob(id: { eq: $id }) {
+      id
+      excerpt
+      content
+      title
+      seo {
+          title
+          metaDesc
+      }
+      featuredImage {
+        node {
+          altText
+          localFile {
+            childImageSharp {
+              fluid(maxWidth: 1000, quality: 100) {
+                ...GatsbyImageSharpFluid_tracedSVG
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
