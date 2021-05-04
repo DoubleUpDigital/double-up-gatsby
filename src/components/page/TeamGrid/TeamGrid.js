3cd81c26c5ea6e5b9a16daa704b629ee9daf8c3e
Justin Radomski <justin@justinradomski.me>
2021-05-04 01:35:14 -0400

    Fixed tabs

--- src/components/page/TeamGrid/TeamGrid.js
+++ src/components/page/TeamGrid/TeamGrid.js
@@ -7,64 +7,64 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLongArrowRight } from '@fortawesome/pro-regular-svg-icons'
 
 const TeamGrid = data => {
-    const people = useStaticQuery(graphql `
-        {
-            allWpTeamMember(sort: {fields: date, order: ASC}){
-                nodes {
-                    teamMemberDetails {
-                        email
-                        phoneNumber
-                        title
-                    }
-                    title
-                    uri
-                    featuredImage {
-                        node {
-                            localFile {
-                                childImageSharp {
-                                    gatsbyImageData(
-                                        layout: CONSTRAINED
-                                        placeholder: BLURRED
-                                        quality: 80
-                                        formats: [AUTO, WEBP]
-                                        outputPixelDensities: [1.5, 2]
-                                        width: 231
-                                        height: 231
-                                        transformOptions: {cropFocus: ATTENTION}
-                                    )
-                                }
-                            }
-                        }
-                    }
+  const people = useStaticQuery(graphql `
+    {
+      allWpTeamMember(sort: {fields: date, order: ASC}){
+        nodes {
+          teamMemberDetails {
+            email
+            phoneNumber
+            title
+          }
+          title
+          uri
+          featuredImage {
+            node {
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(
+                    layout: CONSTRAINED
+                    placeholder: BLURRED
+                    quality: 80
+                    formats: [AUTO, WEBP]
+                    outputPixelDensities: [1.5, 2]
+                    width: 231
+                    height: 231
+                    transformOptions: {cropFocus: ATTENTION}
+                  )
                 }
+              }
             }
+          }
         }
+      }
+    }
 
-        `)
+    `)
   return (
 		<section className={styles.teamGrid}>
-            <div className="container container--slider">
-                <span className={`${styles.teamGrid__tag} tag`}>{data.sectionLabel}</span>
-                <h2 className={styles.teamGrid__heading}>{data.heading}</h2>
+      <div className="container container--slider">
+        <span className={`${styles.teamGrid__tag} tag`}>{data.sectionLabel}</span>
+        <h2 className={styles.teamGrid__heading}>{data.heading}</h2>
 
-                <div className={`${styles.teamGrid__members}`}>
-                {people.allWpTeamMember.nodes.map((teamMember,i) => (
-                      <div className={`${styles.teamGrid__member}`} key={'teamMember_' + i}>
-                          <Link className={`${styles.teamGrid__member_link}`} to={teamMember.uri}>
-                              <GatsbyImage
-                                  className={`${styles.teamGrid__member_image}`}
-                                  image={teamMember.featuredImage.node.localFile.childImageSharp.gatsbyImageData} />
-                              <div className={`${styles.teamGrid__member_text}`}>
-                                  <div className={`${styles.teamGrid__member_name}`}>{teamMember.title}</div>
-                                  <div className={`${styles.teamGrid__member_title}`}>{teamMember.teamMemberDetails.title}</div>
-                                  <div className={`${styles.teamGrid__icon}`}><FontAwesomeIcon icon={faLongArrowRight}/></div>
-                              </div>
-                          </Link>
-                      </div>
-                ))}
-
+        <div className={`${styles.teamGrid__members}`}>
+        {people.allWpTeamMember.nodes.map((teamMember,i) => (
+            <div className={`${styles.teamGrid__member}`} key={'teamMember_' + i}>
+              <Link className={`${styles.teamGrid__member_link}`} to={teamMember.uri}>
+                <GatsbyImage
+                  className={`${styles.teamGrid__member_image}`}
+                  image={teamMember.featuredImage.node.localFile.childImageSharp.gatsbyImageData} />
+                <div className={`${styles.teamGrid__member_text}`}>
+                  <div className={`${styles.teamGrid__member_name}`}>{teamMember.title}</div>
+                  <div className={`${styles.teamGrid__member_title}`}>{teamMember.teamMemberDetails.title}</div>
+                  <div className={`${styles.teamGrid__icon}`}><FontAwesomeIcon icon={faLongArrowRight}/></div>
                 </div>
+              </Link>
             </div>
+        ))}
+
+        </div>
+      </div>
 		</section>
   )
 }
