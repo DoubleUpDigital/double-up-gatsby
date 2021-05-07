e9fc40556b4a1836fc16b41071c053cc7ac9f0bf
Justin Radomski <justin@justinradomski.me>
2021-05-07 13:03:57 -0400

    Linked entire job list item

--- src/components/page/JobList/JobList.js
+++ src/components/page/JobList/JobList.js
@@ -41,23 +41,19 @@ const JobList = data => {
       <div className="container">
         <div className={`${styles.jobList__list} margin-fix`}>
           {jobs.allWpJob.nodes.map((job, i) => (
-            <div className={styles.jobList__list_item} key={'job_' + i}>
-              <Link to={job.uri} className={styles.jobList__list_item_title}>
+            <Link to={job.uri} className={styles.jobList__list_item} key={'job_' + i}>
+              <h3 className={styles.jobList__list_item_title}>
                 {job.title}
-              </Link>
+              </h3>
               <div className={styles.jobList__list_item_tags}>
                 {job.jobsType && <span className={`${styles.jobList__list_item_tags_tag} tag tag--gray tag--small`}><FontAwesomeIcon icon={faClock} /> {job.jobsType}</span>}
                 {job.jobsCity && <span className={`${styles.jobList__list_item_tags_tag} tag tag--gray tag--small`}><FontAwesomeIcon icon={faMapMarkerAlt} /> {job.jobsCity}, {job.jobsState}</span>}
                 {job.jobsDepartment && <span className={`${styles.jobList__list_item_tags_tag} tag tag--purple tag--small`}><FontAwesomeIcon icon={faUsersClass} /> {job.jobsDepartment}</span>}
               </div>
-              <div className={styles.jobList__list_item_link}>
-                <Link to={job.uri} className="button button--inverted">
-    							<span className="button__orb">
-    								<FontAwesomeIcon icon={faLongArrowRight} />
-    							</span>
-    						</Link>
-              </div>
-            </div>
+							<span className={`button__orb ${styles.jobList__list_item_orb}`}>
+								<FontAwesomeIcon icon={faLongArrowRight} />
+							</span>
+            </Link>
           ))}
         </div>
       </div>
