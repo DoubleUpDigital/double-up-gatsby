36717a30ed0cd3eab63d7e412b2c51458eef444e
Avery Williams <averyhwilliams@yahoo.com>
2021-05-11 13:39:52 -0400

    Working on SubscribeBar component.

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,28 +41,28 @@ module.exports = {
       },
     },
 
-    // {
-    //   resolve: 'gatsby-source-gravityforms',
-    //   options: {
-    //     // Base URL needs to include protocol (http/https)
-    //     baseUrl: process.env.WP_URL,
-    //     // Gravity Forms API
-    //     api: {
-    //       key: process.env.CONSUMER_KEY,
-    //       secret: process.env.CONSUMER_SECRET,
-    //     },
-    //     // Set to true to enable selfsigned certs in development mode
-    //     allowSelfSigned: false,
-    //     ignoreFields: [
-    //       // Top level fields within the Gravity Forms return
-    //       // to ignore.
-    //       // Default ignore is 'notifications'. To keep this
-    //       // as set, remove the ignoreFields setting from here.
-    //       // If adding more fields, you will need to include
-    //       // notifications to ensure it is ignored.
-    //     ],
-    //   },
-    // },
+    {
+      resolve: 'gatsby-source-gravityforms',
+      options: {
+        // Base URL needs to include protocol (http/https)
+        baseUrl: process.env.WP_URL,
+        // Gravity Forms API
+        api: {
+          key: process.env.CONSUMER_KEY,
+          secret: process.env.CONSUMER_SECRET,
+        },
+        // Set to true to enable selfsigned certs in development mode
+        allowSelfSigned: false,
+        ignoreFields: [
+          // Top level fields within the Gravity Forms return
+          // to ignore.
+          // Default ignore is 'notifications'. To keep this
+          // as set, remove the ignoreFields setting from here.
+          // If adding more fields, you will need to include
+          // notifications to ensure it is ignored.
+        ],
+      },
+    },
 
     /**
      * We need this plugin so that it adds the "File.publicURL" to our site
--- src/components/page/SubscribeBar/SubscribeBar.js
+++ src/components/page/SubscribeBar/SubscribeBar.js
@@ -1,19 +1,52 @@
 import React from 'react'
-import * as styles from "./callout.module.scss"
-import { Link } from "gatsby"
+import * as styles from "./subscribeBar.module.scss"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import GravityFormForm from 'gatsby-gravityforms-component'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLongArrowRight } from '@fortawesome/pro-regular-svg-icons'
 
+
+
 const SubscribeBar = data => {
-  return (
+    const AllGravityData = () => {
+        const { AllGfForm } = useStaticQuery(
+            graphql`
+                query {
+                    allGfForm {
+                        edges {
+                            node {
+                                ...GravityFormComponent
+                            }
+                        }
+                    }
+                }
+            `
+        )
+        return AllGfForm
+    }
+    function handleError({values, error, reset}) {
+        //handle error
+    }
+    function handleSuccess({values, reset, confirmations}) {
+        //handle success
+    }
+    return (
+
 		<section className={`component ${styles.subscribeBar}`}>
-			<div className="container container--small">
-				{data.sectionLabel && <span className={`tag component__tag ${styles.callout__tag}`}>{data.sectionLabel}</span>}
-				<h2 className={`component__heading ${styles.callout__heading}`}>{data.heading}</h2>
+			<div className="container container--slider">
+				{data.sectionLabel && <span className={`tag component__tag ${styles.subscribeBar__tag}`}>{data.sectionLabel}</span>}
+				<h2 className={`component__heading ${styles.subscribeBar__heading}`}>{data.heading}</h2>
+                <GravityFormForm
+                id={4}
+                formData={AllGravityData()}
+                // lambda={process.env.LAMBDA_ENDPOINT}
+                successCallback={handleSuccess}
+                errorCallback={handleError}
+                ></GravityFormForm>
 			</div>
 		</section>
-  )
+    )
 }
 
 export default SubscribeBar
