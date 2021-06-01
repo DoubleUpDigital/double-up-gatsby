9243aab77434da989a96dc1ef2e0ec4f42f8ad15
Avery Williams <averyhwilliams@yahoo.com>
2021-06-01 15:44:01 -0400

    Trying to remove gf again.

--- src/components/page/SubscribeBar/SubscribeBar.js
+++ src/components/page/SubscribeBar/SubscribeBar.js
@@ -1,30 +1,13 @@
 import React from 'react'
 import * as styles from "./subscribeBar.module.scss"
-import { useStaticQuery, graphql, Link } from "gatsby"
-// import GravityFormForm from 'gatsby-gravityforms-component'
+import { Link } from "gatsby"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLongArrowRight } from '@fortawesome/pro-regular-svg-icons'
 
 
-
 const SubscribeBar = data => {
-    // const AllGravityData = () => {
-    //     const { AllGfForm } = useStaticQuery(
-    //         graphql`
-    //             query {
-    //                 allGfForm {
-    //                     edges {
-    //                         node {
-    //                             ...GravityFormComponent
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         `
-    //     )
-    //     return AllGfForm
-    // }
+
     function handleError({values, error, reset}) {
         //handle error
     }
@@ -37,13 +20,7 @@ const SubscribeBar = data => {
 			<div className="container container--slider">
 				{data.sectionLabel && <span className={`tag component__tag ${styles.subscribeBar__tag}`}>{data.sectionLabel}</span>}
 				<h2 className={`component__heading ${styles.subscribeBar__heading}`}>{data.heading}</h2>
-                // <GravityFormForm
-                // id={4}
-                // formData={AllGravityData()}
-                // // lambda={process.env.LAMBDA_ENDPOINT}
-                // successCallback={handleSuccess}
-                // errorCallback={handleError}
-                // ></GravityFormForm>
+            
 			</div>
 		</section>
     )
