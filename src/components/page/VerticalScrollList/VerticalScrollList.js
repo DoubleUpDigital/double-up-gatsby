819270f544ef32b84ff970e203382e779ece5a49
Avery Williams <averyhwilliams@yahoo.com>
2021-06-14 14:18:24 -0400

    Made animation edits to VerticalScrollList component.

--- src/components/page/VerticalScrollList/VerticalScrollList.js
+++ src/components/page/VerticalScrollList/VerticalScrollList.js
@@ -8,11 +8,38 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck } from '@fortawesome/pro-regular-svg-icons'
 library.add(faCheck);
 
+// const Chunk = data => {
+//
+// }
+
 const VerticalScrollList = data => {
+
+  // const chunkSize = 4;
+  // const arr = {data.listItems.map((item) => (
+  //   {item.listItem}
+  // ))}
+  // const groups = arr.map((e, i) => {
+  //      return i % chunkSize === 0 ? arr.slice(i, i + chunkSize) : null;
+  // }).filter(e => { return e; });
+  // console.log({arr, groups})
+
   return (
 		<section className="verticalList">
       <div className="verticalList__slideshow verticalList__slideshow1">
         <div className="verticalList__items">
+          {data.listItems.map((item,i) => (
+            <div className="verticalList__items_item" key={'listItem_' + i}>{item.listItem}</div>
+          ))}
+          {data.listItems.map((item,i) => (
+            <div className="verticalList__items_item" key={'listItem_' + i}>{item.listItem}</div>
+          ))}
+        </div>
+      </div>
+      <div className="verticalList__slideshow verticalList__slideshow2">
+        <div className="verticalList__items">
+          {data.listItems.map((item,i) => (
+            <div className="verticalList__items_item" key={'listItem_' + i}>{item.listItem}</div>
+          ))}
           {data.listItems.map((item,i) => (
             <div className="verticalList__items_item" key={'listItem_' + i}>{item.listItem}</div>
           ))}
@@ -25,8 +52,21 @@ const VerticalScrollList = data => {
           <div className="verticalList__content" dangerouslySetInnerHTML={{ __html:data.content }}></div>
         </div>
       </div>
-      <div className="verticalList__slideshow verticalList__slideshow2">
+      <div className="verticalList__slideshow verticalList__slideshow3">
         <div className="verticalList__items">
+          {data.listItems.map((item,i) => (
+            <div className="verticalList__items_item" key={'listItem_' + i}>{item.listItem}</div>
+          ))}
+          {data.listItems.map((item,i) => (
+            <div className="verticalList__items_item" key={'listItem_' + i}>{item.listItem}</div>
+          ))}
+        </div>
+      </div>
+      <div className="verticalList__slideshow verticalList__slideshow4">
+        <div className="verticalList__items">
+          {data.listItems.map((item,i) => (
+            <div className="verticalList__items_item" key={'listItem_' + i}>{item.listItem}</div>
+          ))}
           {data.listItems.map((item,i) => (
             <div className="verticalList__items_item" key={'listItem_' + i}>{item.listItem}</div>
           ))}
