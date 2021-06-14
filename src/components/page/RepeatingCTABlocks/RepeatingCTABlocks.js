e7c805d31c3eb250983ed35868414a464e390189
Avery Williams <averyhwilliams@yahoo.com>
2021-06-14 15:11:48 -0400

    Fixed naming issue on RepeatingCtaBlocks component.

--- src/components/page/RepeatingCTABlocks/RepeatingCTABlocks.js
+++ src/components/page/RepeatingCTABlocks/RepeatingCTABlocks.js
@@ -1,27 +1,27 @@
 import React from 'react'
-import "./repeatingCTABlocks.scss"
+import "./repeatingCtaBlocks.scss"
 import { Link } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLongArrowRight } from '@fortawesome/pro-regular-svg-icons'
 
-const RepeatingCTABlocks = data => {
+const RepeatingCtaBlocks = data => {
   return (
-		<section className="repeatingCTABlocks">
+		<section className="RepeatingCtaBlocks">
 
       <div className="container">
-        <div className="repeatingCTABlocks__blocks">
+        <div className="RepeatingCtaBlocks__blocks">
 					{data.ctaBlocks.map((block,i) => (
 
-              <div className="repeatingCTABlocks__block">
+              <div className="RepeatingCtaBlocks__block">
                 <GatsbyImage
-                  className="repeatingCTABlocks__graphic"
+                  className="RepeatingCtaBlocks__graphic"
                   image={block.graphic.localFile.childImageSharp.gatsbyImageData}
                   alt={block.graphic.altText} />
-                <div className="repeatingCTABlocks__heading">{block.heading}</div>
-                <div className="repeatingCTABlocks__content">{block.content}</div>
-                <Link to={block.button.url} key={'block_' + i} className="repeatingCTABlocks__link" target={block.button.target}>{block.button.title}</Link>
+                <div className="RepeatingCtaBlocks__heading">{block.heading}</div>
+                <div className="RepeatingCtaBlocks__content">{block.content}</div>
+                <Link to={block.button.url} key={'block_' + i} className="RepeatingCtaBlocks__link" target={block.button.target}>{block.button.title}</Link>
               </div>
 
 					))}
@@ -32,4 +32,4 @@ const RepeatingCTABlocks = data => {
   )
 }
 
-export default RepeatingCTABlocks
+export default RepeatingCtaBlocks
--- src/components/page/RepeatingCTABlocks/index.js
+++ src/components/page/RepeatingCTABlocks/index.js
@@ -1,1 +1,1 @@
-export { default } from './RepeatingCTABlocks'
+export { default } from './RepeatingCtaBlocks'
