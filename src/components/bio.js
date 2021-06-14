dc142dce36b80e4ee9720baf867fdf9524292755
Avery Williams <averyhwilliams@yahoo.com>
2021-06-14 18:25:51 -0400

    Built and styled bio box on blog single.

--- src/components/bio.js
+++ src/components/bio.js
@@ -7,39 +7,52 @@
 
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
+
+import * as styles from "../templates/blog-post.module.scss"
 
 const Bio = () => {
   const { author } = useStaticQuery(graphql`
     query BioQuery {
       # if there was more than one user, this would need to be filtered
       author: wpUser {
-        firstName
-        lastName
-        description
-        avatar {
-          url
+        userOptions {
+          teamMember {
+            ... on WpTeamMember {
+              content
+              title
+              featuredImage {
+                node {
+                  localFile {
+                    childImageSharp {
+                      gatsbyImageData
+                    }
+                  }
+                }
+              }
+            }
+          }
         }
       }
+
     }
   `)
 
-  const avatarUrl = author?.avatar?.url
-
   return (
-    <div className="bio">
-      {avatarUrl && (
-        <img
-          alt={author?.firstName || ``}
-          className="bio-avatar"
-          src={avatarUrl}
-        />
-      )}
-      {author?.firstName && (
-          <>
-          <h3>{author.firstName} {author.lastName}</h3>
-          <div>{author?.description || null}</div>
-          </>
-      )}
+    <div className={`${styles.bio}`}>
+      <div className="container container--medium-2">
+        {author.userOptions.teamMember.map((member, i) => (
+          <div className={`${styles.bio__inner}`}>
+            <GatsbyImage
+                image={member.featuredImage.node.localFile.childImageSharp.gatsbyImageData}
+                className={`${styles.bio__headshot}`} />
+            <div className={`${styles.bio__content}`}>
+              <div className={`${styles.bio__name}`}>{member.title}</div>
+              <div dangerouslySetInnerHTML={{__html: member.content}}></div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
