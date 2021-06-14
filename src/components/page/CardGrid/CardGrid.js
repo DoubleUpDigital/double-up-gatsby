f3e54a89af785eccdbba6248cee3a3ae38d0236e
Justin Radomski <justin@doubleupdigital.com>
2021-06-14 10:59:41 -0400

    Removed module css from cardGrid

--- src/components/page/CardGrid/CardGrid.js
+++ src/components/page/CardGrid/CardGrid.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import * as styles from "./cardGrid.module.scss"
+import "./cardGrid.scss"
 import { Link } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 
@@ -8,30 +8,30 @@ import { faLongArrowRight } from '@fortawesome/pro-regular-svg-icons'
 
 const CardGrid = data => {
   return (
-		<section className={styles.cardGrid}>
+		<section className="cardGrid">
 			<div className="container container--small">
-				<span className={`${styles.cardGrid__tag} tag`}>{data.sectionLabel}</span>
-				<h2 className={styles.cardGrid__heading}>{data.heading}</h2>
-				<div className={`${styles.cardGrid__content} margin-fix`} dangerouslySetInnerHTML={{ __html:data.content }}></div>
-            </div>
+				<span className="cardGrid__tag tag">{data.sectionLabel}</span>
+				<h2 className="cardGrid__heading">{data.heading}</h2>
+				<div className="cardGrid__content margin-fix" dangerouslySetInnerHTML={{ __html:data.content }}></div>
+      </div>
 
-            <div className="container">
-                <div className={`${styles.cardGrid__cards} card-row`}>
+      <div className="container">
+        <div className="card-row cardGrid__cards">
 					{data.cards.map((card,i) => (
-						<Link to={card.cardLink.url} key={'card_' + i} className={`${styles.cardGrid__card} card`}>
-                            <div className={`${styles.cardGrid__card_main}`}>
-                                <GatsbyImage 
-                                    className={`${styles.cardGrid__card_image}`} 
-                                    image={card.cardImage.localFile.childImageSharp.gatsbyImageData}
-                                    alt={card.cardImage.altText} />
-                                <div className={`${styles.cardGrid__card_title}`}>{card.cardTitle}</div>
-                                <div className={`${styles.cardGrid__card_content}`}>{card.cardContent}</div>
-                            </div>
-							<span className={`${styles.cardGrid__card_link} text-${card.cardColor} fake-button`}>Learn More <FontAwesomeIcon icon={faLongArrowRight} /></span>
+						<Link to={card.cardLink.url} key={'card_' + i} className="card cardGrid__card">
+              <div className="cardGrid__card-main">
+                <GatsbyImage
+                  className="cardGrid__card-image"
+                  image={card.cardImage.localFile.childImageSharp.gatsbyImageData}
+                  alt={card.cardImage.altText} />
+                <div className="cardGrid__card-title">{card.cardTitle}</div>
+                <div className="cardGrid__card-content">{card.cardContent}</div>
+              </div>
+							<span className={`cardGrid__card-link text-${card.cardColor} fake-button`}>Learn More <FontAwesomeIcon icon={faLongArrowRight} /></span>
 						</Link>
 					))}
-				</div>
-            </div>
+			  </div>
+      </div>
 
 		</section>
   )
