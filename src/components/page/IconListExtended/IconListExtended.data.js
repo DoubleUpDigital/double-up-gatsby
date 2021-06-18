3e60318ce82d69583614c62cb84ff914e6ddf785
Avery Williams <averyhwilliams@yahoo.com>
2021-06-18 12:56:52 -0400

    Made design edits to IconListExtended component.

--- src/components/page/IconListExtended/IconListExtended.data.js
+++ src/components/page/IconListExtended/IconListExtended.data.js
@@ -1,6 +1,8 @@
 module.exports = () => {
 	return `
+    tag
 		heading
+    content
 		listItems {
 			itemText
 			itemIcon {
--- src/components/page/IconListExtended/IconListExtended.js
+++ src/components/page/IconListExtended/IconListExtended.js
@@ -6,23 +6,28 @@ import { Link } from "gatsby"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck, faLongArrowRight } from '@fortawesome/pro-regular-svg-icons'
+import { faCheckCircle } from '@fortawesome/pro-solid-svg-icons'
 library.add(faCheck);
 
 const IconListExtended = data => {
   return (
 		<section className="IconListExtended sideways-heading-section light-blue-section">
             <div className="container container--medium-2">
-            <h2 class="sideways-heading">{data.heading}</h2>
-            <div className="IconListExtended__list sideways-heading-content">
-                {data.listItems.map((item,i) => (
-                    <div className="IconListExtended__list-item" key={'listItem_' + i}>
-                        <span className="IconListExtended__list-icon">
-                            <GatsbyImage image={item.itemIcon.localFile.childImageSharp.gatsbyImageData} />
-                        </span>
-                        <span className="IconListExtended__list-text" dangerouslySetInnerHTML={{__html:item.itemText}}></span>
-                    </div>
-                ))}
-            </div>
+              <div className="IconListExtended__top">
+                <span className="IconListExtended__tag tag">{data.tag}</span>
+                <h2>{data.heading}</h2>
+                <div>{data.content}</div>
+              </div>
+              <div className="IconListExtended__list">
+                  {data.listItems.map((item,i) => (
+                      <div className="IconListExtended__list-item" key={'listItem_' + i}>
+                          <span className="IconListExtended__list-icon">
+                              <FontAwesomeIcon icon={faCheckCircle} />
+                          </span>
+                          <span className="IconListExtended__list-text" dangerouslySetInnerHTML={{__html:item.itemText}}></span>
+                      </div>
+                  ))}
+              </div>
             </div>
 		</section>
   )
