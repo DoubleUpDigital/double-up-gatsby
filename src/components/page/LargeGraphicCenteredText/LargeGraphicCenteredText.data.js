175121b92533abeae5cac1cc4574cb1bde99e5d2
Avery Williams <averyhwilliams@yahoo.com>
2021-06-18 16:53:35 -0400

    Fixed image and background issues on LargeGraphicCenteredText component.

--- src/components/page/LargeGraphicCenteredText/LargeGraphicCenteredText.data.js
+++ src/components/page/LargeGraphicCenteredText/LargeGraphicCenteredText.data.js
@@ -13,11 +13,17 @@ module.exports = () => {
             formats: [AUTO, WEBP]
             outputPixelDensities: [1.5, 2]
             width: 972
-            height: 659
+            height: 567
             transformOptions: {cropFocus: NORTH}
 					)
 				}
 			}
 		}
+    background {
+      hasBackground
+      squiggleTop
+      squiggleBottom
+      lastComponent
+    }
 	`
 }
--- src/components/page/LargeGraphicCenteredText/LargeGraphicCenteredText.js
+++ src/components/page/LargeGraphicCenteredText/LargeGraphicCenteredText.js
@@ -6,27 +6,30 @@ import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLongArrowRight } from '@fortawesome/pro-regular-svg-icons'
 
+import Squiggle from "../../abstracts/Squiggle"
+
 const LargeGraphicCenteredText = data => {
   return (
-		<section className="LargeGraphicCenteredText">
-            <StaticImage
-              className="LargeGraphicCenteredText__bg"
-              src="../../../ui/light-blue-squiggle-top.png"
-              placeholder="tracedSVG"
-              quality="100"
-              alt=""
-              style={{position: "absolute"}} />
-			<div className="LargeGraphicCenteredText__inner">
-                <div className="container container--medium-2">
-                    <GatsbyImage
-                        className="LargeGraphicCenteredText__graphic"
-                        image={data.graphic.localFile.childImageSharp.gatsbyImageData}
-                        alt={data.altText} />
-                        <h2>{data.heading}</h2>
-                        <div className="LargeGraphicCenteredText__content" dangerouslySetInnerHTML={{ __html:data.content }}></div>
-                </div>
-            </div>
-		</section>
+    <>
+    {(data.background.squiggleTop && data.background.hasBackground) && <Squiggle type={data.background.squiggleTop} />}
+    <section className={`component LargeGraphicCenteredText
+      ${data.background.hasBackground ? 'component--with-background'  : ""}
+      ${(data.background.squiggleTop && data.background.hasBackground) ? 'component--squiggleTop'  : ""}
+      ${(data.background.squiggleBottom && data.background.hasBackground) ? 'component--squiggleBottom'  : ""}
+      ${(data.background.lastComponent && data.background.hasBackground) ? 'component--last'  : ""}`}>
+      <div className="LargeGraphicCenteredText__inner">
+        <div className="container container--medium-2">
+          <GatsbyImage
+            className="LargeGraphicCenteredText__graphic"
+            image={data.graphic.localFile.childImageSharp.gatsbyImageData}
+            alt={data.altText} />
+          <h2>{data.heading}</h2>
+          <div className="LargeGraphicCenteredText__content" dangerouslySetInnerHTML={{ __html:data.content }}></div>
+        </div>
+      </div>
+    </section>
+    {(data.background.squiggleBottom && data.background.hasBackground) && <Squiggle type={data.background.squiggleBottom} />}
+    </>
   )
 }
 
