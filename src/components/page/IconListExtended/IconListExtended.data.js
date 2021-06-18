425739faee96db94534447fe156a8d8243f9d5ac
Avery Williams <averyhwilliams@yahoo.com>
2021-06-18 15:42:49 -0400

    Update IconListExtended.data.js

--- src/components/page/IconListExtended/IconListExtended.data.js
+++ src/components/page/IconListExtended/IconListExtended.data.js
@@ -5,18 +5,6 @@ module.exports = () => {
     content
 		listItems {
 			itemText
-			itemIcon {
-				localFile {
-					childImageSharp {
-						gatsbyImageData(
-							width: 40
-							quality: 100
-							placeholder: BLURRED
-							formats: [AUTO, WEBP]
-						)
-					}
-				}
-			}
 		}
 	`
 }
