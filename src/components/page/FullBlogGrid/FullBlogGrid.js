e0bfa6c6b7a58eec7b9e281efada816a82042674
Avery Williams <averyhwilliams@yahoo.com>
2021-06-25 14:22:56 -0400

    Update FullBlogGrid.js

--- src/components/page/FullBlogGrid/FullBlogGrid.js
+++ src/components/page/FullBlogGrid/FullBlogGrid.js
@@ -9,39 +9,42 @@ import { faLongArrowRight } from '@fortawesome/pro-regular-svg-icons'
 const FullBlogGrid = data => {
   const fullPosts = useStaticQuery(graphql `
     {
-        allWpPost {
-          nodes {
-            featuredImage {
-              node {
-                localFile {
-                  childImageSharp {
-                    gatsbyImageData(
-                      layout: CONSTRAINED
-                      placeholder: BLURRED
-                      quality: 70
-                      formats: [AUTO, WEBP]
-                      outputPixelDensities: [1.5, 2]
-                      width: 350
-                      height: 225
-                      transformOptions: {cropFocus: ATTENTION}
-                    )
+        allWpPost(sort: {order: DESC, fields: date}) {
+          edges {
+            node {
+              featuredImage {
+                node {
+                  localFile {
+                    childImageSharp {
+                      gatsbyImageData(
+                        layout: CONSTRAINED
+                        placeholder: BLURRED
+                        quality: 70
+                        formats: [AUTO, WEBP]
+                        outputPixelDensities: [1.5, 2]
+                        width: 350
+                        height: 225
+                        transformOptions: {cropFocus: ATTENTION}
+                      )
+                    }
                   }
                 }
               }
-            }
-            categories {
-              nodes {
-                name
-                uri
+              categories {
+                nodes {
+                  name
+                  slug
+                  uri
+                }
               }
-            }
-            title
-            uri
-            date(formatString: "d MMMM Y")
-            author {
-              node {
-                uri
-                name
+              title
+              uri
+              date(formatString: "DD MMMM, YYYY")
+              author {
+                node {
+                  uri
+                  name
+                }
               }
             }
           }
@@ -49,6 +52,7 @@ const FullBlogGrid = data => {
         allWpCategory {
             nodes {
                 name
+                slug
                 posts {
                   nodes {
                     title
@@ -90,60 +94,70 @@ const FullBlogGrid = data => {
     }
 
     `)
+    //
+    // // Array of all news articles
+    //   const allNews = fullPosts.allWpCategory.nodes
+    //
+    //   // State for the list
+    //   const [list, setList] = useState([...allNews.slice(0, 9)])
+    //
+    //   // State to trigger oad more
+    //   const [loadMore, setLoadMore] = useState(false)
+    //
+    //   // State of whether there is more to load
+    //   const [hasMore, setHasMore] = useState(allNews.length > 9)
+    //
+    //   // Load more button click
+    //   const handleLoadMore = () => {
+    //     setLoadMore(true)
+    //   }
+    //
+    //   // Handle loading more articles
+    //   useEffect(() => {
+    //     if (loadMore && hasMore) {
+    //       const currentLength = list.length
+    //       const isMore = currentLength < allNews.length
+    //       const nextResults = isMore
+    //         ? allNews.slice(currentLength, currentLength + 9)
+    //         : []
+    //       setList([...list, ...nextResults])
+    //       setLoadMore(false)
+    //     }
+    //   }, [loadMore, hasMore]) //eslint-disable-line
+    //
+    //   //Check if there is more
+    //   useEffect(() => {
+    //     const isMore = list.length < allNews.length
+    //     setHasMore(isMore)
+    //   }, [list]) //eslint-disable-line
 
-// Array of all news articles
-  const allNews = fullPosts.allWpCategory.nodes
 
-  // State for the list
-  const [list, setList] = useState([...allNews.slice(0, 9)])
-
-  // State to trigger oad more
-  const [loadMore, setLoadMore] = useState(false)
-
-  // State of whether there is more to load
-  const [hasMore, setHasMore] = useState(allNews.length > 9)
-
-  // Load more button click
-  const handleLoadMore = () => {
-    setLoadMore(true)
-  }
-
-  // Handle loading more articles
-  useEffect(() => {
-    if (loadMore && hasMore) {
-      const currentLength = list.length
-      const isMore = currentLength < allNews.length
-      const nextResults = isMore
-        ? allNews.slice(currentLength, currentLength + 9)
-        : []
-      setList([...list, ...nextResults])
-      setLoadMore(false)
+    // Create state for our posts list and the selected value
+    const [posts, setPosts] = useState(fullPosts.allWpPost.edges)
+    const [selected, setSelected] = useState("all")
+    // Our function that does the filtering
+    const filterPosts = value => {
+      let posts = fullPosts.allWpPost.edges
+      if (value !== "all") {
+        posts = fullPosts.allWpPost.edges.filter(post =>
+          post.node.categories.nodes.find(category => category.slug === value)
+        )
+      }
+      setSelected(value)
+      setPosts(posts)
     }
-  }, [loadMore, hasMore]) //eslint-disable-line
-
-  //Check if there is more
-  useEffect(() => {
-    const isMore = list.length < allNews.length
-    setHasMore(isMore)
-  }, [list]) //eslint-disable-line
 
   return (
       <section className={styles.fullBlogGrid}>
 
           <div className={`container container--wide`}>
               <h3>{data.fullBlogTitle}</h3>
-              <div className={`${styles.fullBlogGrid__catRow}`}>
-                  <span key={'category_all'} className={`${styles.fullBlogGrid__category}`}>All</span>
+              <select onChange={e => filterPosts(e.target.value)} value={selected} className={`${styles.fullBlogGrid__catRow}`}>
+                  <option key={'category_all'} value="all" className={`${styles.fullBlogGrid__category}`}>All</option>
                   {fullPosts.allWpCategory.nodes.map((category, i) => (
                       <>
-                      <script
-                          dangerouslySetInnerHTML={{__html: `
-                            console.log('Plain JavaScript inside Gatsby!');
-                            `
-                          }}
-                        />
                       {category.name === "Uncategorized" ? '' :
-                      <span className={`${styles.fullBlogGrid__category}
+                      <option value={category.slug} className={`${styles.fullBlogGrid__category}
                       ${category.name === "Announcements" ? styles.fullBlogGrid__category_announcements :
                       category.name === "Business" ? styles.fullBlogGrid__category_business :
                       category.name === "Design" ? styles.fullBlogGrid__category_design :
@@ -152,60 +166,49 @@ const FullBlogGrid = data => {
                       category.name === "SEO" ? styles.fullBlogGrid__category_seo :
                       category.name === "Social Media" ? styles.fullBlogGrid__category_socialMedia :
                       category.name === "Web Development" ? styles.fullBlogGrid__category_webDevelopment :
-                      category.name === "WordPress" ? styles.fullBlogGrid__category_wordpress : ""}`} key={'category_' + i}>{category.name}</span> }
+                      category.name === "WordPress" ? styles.fullBlogGrid__category_wordpress : ""}`} key={'category_' + i}>{category.name}</option> }
                       </>
+                  ))}
+              </select>
+              <div className={`${styles.fullBlogGrid__flex}`}>
+                {fullPosts.allWpPost.edges.map((fullPost,i) => (
+                    <>
+                      <div className={`${styles.fullBlogGrid__post}`} key={'post_' + i}>
+                        <span className={`${styles.fullBlogGrid__post_cats}`}>
+                        {fullPost.node.categories.nodes.map((cat,i) => (
+                          <>
+                            <span className={`${styles.fullBlogGrid__post_cat}
+                              ${cat.name === "Announcements" ? styles.fullBlogGrid__post_cat_announcements :
+                              cat.name === "Business" ? styles.fullBlogGrid__post_cat_business :
+                              cat.name === "Design" ? styles.fullBlogGrid__post_cat_design :
+                              cat.name === "Digital Marketing" ? styles.fullBlogGrid__post_cat_digitalMarketing :
+                              cat.name === "General" ? styles.fullBlogGrid__post_cat_general :
+                              cat.name === "SEO" ? styles.fullBlogGrid__post_cat_seo :
+                              cat.name === "Social Media" ? styles.fullBlogGrid__post_cat_socialMedia :
+                              cat.name === "Web Development" ? styles.fullBlogGrid__post_cat_webDevelopment :
+                              cat.name === "WordPress" ? styles.fullBlogGrid__post_cat_wordpress : ""}`} key={'cat_' + i}>
+                                {cat.name}
+                            </span>
+                          </>
+                        ))}
+                        </span>
+                        <Link to={fullPost.node.uri}><GatsbyImage
+                          className={`${styles.fullBlogGrid__post_image}`}
+                          image={fullPost.node.featuredImage.node.localFile.childImageSharp.gatsbyImageData}
+                          alt=""
+                          height="400" /></Link>
+                        <div className={`${styles.fullBlogGrid__post_meta}`}>
+                          <span>{fullPost.node.date}</span>
+                          <span className={`${styles.fullBlogGrid__post_separator}`}>•</span>
+                          <span><Link to={fullPost.node.author.node.uri} className={`${styles.fullBlogGrid__post_authorLink}`}>{fullPost.node.author.node.name}</Link></span>
+                        </div>
+                        <h2><Link to={fullPost.node.uri}>{fullPost.node.title}</Link></h2>
+                        <div className={`${styles.fullBlogGrid__post_excerpt}`} dangerouslySetInnerHTML={{__html: fullPost.node.excerpt}}></div>
+                      </div>
+                    </>
                   ))}
               </div>
-                  {allNews.map((fullCat,i) => (
-                      <>
-                      <div className={`${styles.fullBlogGrid__flex}`}>
-                          {fullCat.posts.nodes.map((fullPost, i) => (
-                            <>
-                            <div className={`${styles.fullBlogGrid__post}`} key={'post_' + i}>
-                                <span className={`${styles.fullBlogGrid__post_cats}`}>
-                                    {fullPost.categories.nodes.map((cat,i) => (
-                                        <>
-                                            <span className={`${styles.fullBlogGrid__post_cat}
-                                            ${cat.name === "Announcements" ? styles.fullBlogGrid__post_cat_announcements :
-                                            cat.name === "Business" ? styles.fullBlogGrid__post_cat_business :
-                                            cat.name === "Design" ? styles.fullBlogGrid__post_cat_design :
-                                            cat.name === "Digital Marketing" ? styles.fullBlogGrid__post_cat_digitalMarketing :
-                                            cat.name === "General" ? styles.fullBlogGrid__post_cat_general :
-                                            cat.name === "SEO" ? styles.fullBlogGrid__post_cat_seo :
-                                            cat.name === "Social Media" ? styles.fullBlogGrid__post_cat_socialMedia :
-                                            cat.name === "Web Development" ? styles.fullBlogGrid__post_cat_webDevelopment :
-                                            cat.name === "WordPress" ? styles.fullBlogGrid__post_cat_wordpress : ""}`} key={'cat_' + i}>
-                                                {cat.name}
-                                            </span>
-                                        </>
-                                    ))}
-                                </span>
-                                <Link to={fullPost.uri}><GatsbyImage
-                                    className={`${styles.fullBlogGrid__post_image}`}
-                                    image={fullPost.featuredImage.node.localFile.childImageSharp.gatsbyImageData}
-                                    alt=""
-                                    height="400" /></Link>
-                                <div className={`${styles.fullBlogGrid__post_meta}`}>
-                                    <span>{fullPost.date}</span>
-                                    <span className={`${styles.fullBlogGrid__post_separator}`}>•</span>
-                                    <span><Link to={fullPost.author.node.uri} className={`${styles.fullBlogGrid__post_authorLink}`}>{fullPost.author.node.name}</Link></span>
-                                </div>
-                              <h2><Link to={fullPost.uri}>{fullPost.title}</Link></h2>
-                              <div className={`${styles.fullBlogGrid__post_excerpt}`} dangerouslySetInnerHTML={{__html: fullPost.excerpt}}></div>
-                            </div>
-                            </>
-                          ))}
-                          </div>
-                      </>
-                  ))}
-                <div className={`${styles.fullBlogGrid__loadMore}`}>
-                    {hasMore ? (
-                        <button onClick={handleLoadMore}>Load More</button>
-                        ) : (
-                        <p>No more results</p>
-                    )}
-                </div>
-          </div>
+        </div>
       </section>
   )
 }
