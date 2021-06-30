437caefb990c8589f69f8381432fa94610a73889
Avery Williams <averyhwilliams@yahoo.com>
2021-06-30 16:54:41 -0400

    Finished styling form submission buttons.

--- static/main.js
+++ static/main.js
@@ -1,18 +1,24 @@
 jQuery(document).ready(function($) {
-  // var windowWidth = $( window ).width();
-  // if(windowWidth < 769) {
-  //   $('.footer-col .fl-col-content .footer-content').hide();
-  //   $('.footer-col1 .fl-col-content .footer-heading').click(function() {
-  //   $(this).toggleClass('active');
-  //   $('.footer-col1 .fl-col-content .footer-content').slideToggle();
-  //   });
-  //   $('.footer-col2 .fl-col-content .footer-heading').click(function() {
-  //   $(this).toggleClass('active');
-  //   $('.footer-col2 .fl-col-content .footer-content').slideToggle();
-  //   });
-  //   $('.footer-col3 .fl-col-content .footer-heading').click(function() {
-  //   $(this).toggleClass('active');
-  //   $('.footer-col3 .fl-col-content .footer-content').slideToggle();
-  //   });
-  // }
+  var windowWidth = $( window ).width();
+  if(windowWidth < 769) {
+    $('.site-footer__cols-list .site-footer__list').hide();
+    $('.site-footer__cols-list-1 .site-footer__list-label').click(function() {
+      $(this).toggleClass('active');
+      $('.site-footer__cols-list-1 .site-footer__list').slideToggle();
+    });
+    $('.site-footer__cols-list-2 .site-footer__list-label').click(function() {
+      $(this).toggleClass('active');
+      $('.site-footer__cols-list-2 .site-footer__list').slideToggle();
+    });
+    $('.site-footer__cols-list-3 .site-footer__list-label').click(function() {
+      $(this).toggleClass('active');
+      $('.site-footer__cols-list-3 .site-footer__list').slideToggle();
+    });
+  }
+
+  $('#gravityform--id-2 .gform_footer .gform_button').html('<i class="far fa-long-arrow-right"></i>');
+  $('#gravityform--id-4 .gform_footer .gform_button').html('<i class="far fa-long-arrow-right"></i>');
+
+  $('#gravityform--id-5 .gform_footer .gform_button').append('<span class="orb"><i class="far fa-long-arrow-right"></i></span>');
+  $('#gravityform--id-6 .gform_footer .gform_button').append('<span class="orb"><i class="far fa-long-arrow-right"></i></span>');
 });
