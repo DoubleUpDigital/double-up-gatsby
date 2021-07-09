6bd46e4ebb857a924fe6b7fc4e792e1db734c6d3
Justin Radomski <justin@doubleupdigital.com>
2021-07-09 14:31:14 -0400

    Added animation to contact page

--- src/components/hero/LightWithGraphic/LightWithGraphic.js
+++ src/components/hero/LightWithGraphic/LightWithGraphic.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
 import "./lightWithGraphic.scss"
 import { Link } from "gatsby"
+
+import Lottie from "lottie-react"
+import contactAnimation from "/content/assets/contact.json"
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -9,26 +11,22 @@ import { faLongArrowRight } from '@fortawesome/pro-regular-svg-icons'
 
 const LightWithGraphic = data => {
   return (
-        <section className="heroLight">
-            <GatsbyImage
-            className="heroLight__graphic"
-            image={data.graphic.localFile.childImageSharp.gatsbyImageData}
-            alt=""
-            style={{position: "absolute"}} />
-            <div className="heroLight__content">
-                <div className="container">
-                    <div className="heroLight__inner">
-                        <span className="heroLight__tag tag">{data.title}</span>
-                        <h1 className="heroLight__title">{data.heading}</h1>
-                        <div
-                        className="heroLight__description"
-                        dangerouslySetInnerHTML={{ __html:data.content}}>
-                        </div>
-                        <Link className="heroLight__cta" to={data.ctaLink.url}>{data.ctaLink.title} <FontAwesomeIcon icon={faLongArrowRight} /></Link>
-                    </div>
-                </div>
+    <section className="heroContact">
+      <Lottie className="heroContact__animation" animationData={contactAnimation} renderer="svg" />
+      <div className="heroContact__content">
+        <div className="container">
+          <div className="heroContact__inner">
+            <span className="heroContact__tag tag">{data.title}</span>
+            <h1 className="heroContact__title">{data.heading}</h1>
+            <div
+            className="heroContact__description"
+            dangerouslySetInnerHTML={{ __html:data.content}}>
             </div>
-        </section>
+            <Link className="heroContact__cta" to={data.ctaLink.url}>{data.ctaLink.title} <FontAwesomeIcon icon={faLongArrowRight} /></Link>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
