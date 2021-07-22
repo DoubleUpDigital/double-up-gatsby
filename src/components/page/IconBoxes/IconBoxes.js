68f8fd836fe6ae4d6b104e09c0cc36924a11c0a0
Avery Williams <averyhwilliams@yahoo.com>
2021-07-22 17:23:58 -0400

    Built and styledIconBoxes component.

--- src/components/page/IconBoxes/IconBoxes.js
+++ src/components/page/IconBoxes/IconBoxes.js
@@ -2,25 +2,35 @@ import React from 'react'
 import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
 import * as styles from "./iconBoxes.scss"
 import { Link } from "gatsby"
+import Squiggle from "../../abstracts/Squiggle"
 
 const IconBoxes = data => {
   return (
-		<section className="iconBoxes">
-      <div className="container">
-        <span className={`${styles.iconList__tag} tag`}>{data.tag}</span>
-        <h2>{data.heading}</h2>
-        <div className="iconBoxes__flex">
-          {data.iconBox.map((item,i) => (
-            <div className="iconBoxes__box" key={'item' + i}>
-              <GatsbyImage
-              className="iconBoxes__icon"
-              image={item.boxIcon.localFile.childImageSharp.gatsbyImageData} />
-              <span className="iconBoxes__text">{item.boxText}</span>
-            </div>
-          ))}
+    <>
+      {(data.background.squiggleTop && data.background.hasBackground) && <Squiggle type={data.background.squiggleTop} />}
+  		<section className={`component iconBoxes
+        ${data.leftWithGraphic ? "iconBoxes--leftWithGraphic" : ""}
+        ${data.background.hasBackground ? 'component--with-background'  : ""}
+        ${(data.background.squiggleTop && data.background.hasBackground) ? 'component--squiggleTop'  : ""}
+        ${(data.background.squiggleBottom && data.background.hasBackground) ? 'component--squiggleBottom'  : ""}
+        ${(data.background.lastComponent && data.background.hasBackground) ? 'component--last'  : ""}`}>
+        <div className="container">
+          <span className="tag">{data.tag}</span>
+          <h2 className="iconBoxes__heading">{data.heading}</h2>
+          <div className="iconBoxes__flex">
+            {data.iconBox.map((item,i) => (
+              <div className="iconBoxes__box" key={'item' + i}>
+                <GatsbyImage
+                className="iconBoxes__icon"
+                image={item.boxIcon.localFile.childImageSharp.gatsbyImageData} />
+                <span className="iconBoxes__text">{item.boxText}</span>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-		</section>
+  		</section>
+      {(data.background.squiggleBottom && data.background.hasBackground) && <Squiggle type={data.background.squiggleBottom} />}
+    </>
   )
 }
 
