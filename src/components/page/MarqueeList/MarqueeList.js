d18b76ff5477b5a5527fc1c1ffeb00dcd0e89ffe
Avery Williams <averyhwilliams@yahoo.com>
2021-07-22 16:03:22 -0400

    Fixed gravity forms so submissions work.

--- src/components/page/MarqueeList/MarqueeList.js
+++ src/components/page/MarqueeList/MarqueeList.js
@@ -10,20 +10,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck } from '@fortawesome/pro-regular-svg-icons'
 library.add(faCheck);
 
-// const Chunk = data => {
-//
-// }
-
 const MarqueeList = data => {
-
-  // const chunkSize = 4;
-  // const arr = {data.listItems.map((item) => (
-  //   {item.listItem}
-  // ))}
-  // const groups = arr.map((e, i) => {
-  //      return i % chunkSize === 0 ? arr.slice(i, i + chunkSize) : null;
-  // }).filter(e => { return e; });
-  // console.log({arr, groups})
 
   return (
     <>
@@ -32,7 +19,9 @@ const MarqueeList = data => {
         ${data.background.hasBackground ? 'component--with-background'  : ""}
         ${(data.background.squiggleTop && data.background.hasBackground) ? 'component--squiggleTop'  : ""}
         ${(data.background.squiggleBottom && data.background.hasBackground) ? 'component--squiggleBottom'  : ""}
-        ${(data.background.lastComponent && data.background.hasBackground) ? 'component--last'  : ""}`}>
+        ${(data.background.lastComponent && data.background.hasBackground) ? 'component--last'  : ""}
+        ${!data.sandwichedContent ? 'not-sandwiched'  : ""}`}>
+
         <div className="marqueeList__slideshow marqueeList__slideshow1">
           <div className="marqueeList__items">
             {data.listItems.map((item,i) => (
@@ -43,6 +32,7 @@ const MarqueeList = data => {
             ))}
           </div>
         </div>
+
         <div className="marqueeList__slideshow marqueeList__slideshow2">
           <div className="marqueeList__items">
             {data.listItems.map((item,i) => (
@@ -53,33 +43,52 @@ const MarqueeList = data => {
             ))}
           </div>
         </div>
-        <div className="marqueeList__inner">
-          <div className="container container--small">
-            <span className="component__tag marqueeList__tag tag">{data.tag}</span>
-            <h2 className="component__heading marqueeList__heading">{data.heading}</h2>
-            <div className="component__content marqueeList__content margin-fix" dangerouslySetInnerHTML={{ __html:data.content }}></div>
+
+        {!data.sandwichedContent &&
+          <div className="marqueeList__slideshow marqueeList__slideshow5">
+            <div className="marqueeList__items">
+              {data.listItems.map((item,i) => (
+                <div className="marqueeList__items_item" key={'listItem_' + i}>{item.listItem}</div>
+              ))}
+              {data.listItems.map((item,i) => (
+                <div className="marqueeList__items_item" key={'listItem_' + i}>{item.listItem}</div>
+              ))}
+            </div>
+          </div>}
+
+        {data.sandwichedContent &&
+        <>
+          <div className="marqueeList__inner">
+            <div className="container container--small">
+              <span className="component__tag marqueeList__tag tag">{data.tag}</span>
+              <h2 className="component__heading marqueeList__heading">{data.heading}</h2>
+              <div className="component__content marqueeList__content margin-fix" dangerouslySetInnerHTML={{ __html:data.content }}></div>
+            </div>
           </div>
-        </div>
-        <div className="marqueeList__slideshow marqueeList__slideshow3">
-          <div className="marqueeList__items">
-            {data.listItems.map((item,i) => (
-              <div className="marqueeList__items_item" key={'listItem_' + i}>{item.listItem}</div>
-            ))}
-            {data.listItems.map((item,i) => (
-              <div className="marqueeList__items_item" key={'listItem_' + i}>{item.listItem}</div>
-            ))}
+
+          <div className="marqueeList__slideshow marqueeList__slideshow3">
+            <div className="marqueeList__items">
+              {data.listItems.map((item,i) => (
+                <div className="marqueeList__items_item" key={'listItem_' + i}>{item.listItem}</div>
+              ))}
+              {data.listItems.map((item,i) => (
+                <div className="marqueeList__items_item" key={'listItem_' + i}>{item.listItem}</div>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="marqueeList__slideshow marqueeList__slideshow4">
-          <div className="marqueeList__items">
-            {data.listItems.map((item,i) => (
-              <div className="marqueeList__items_item" key={'listItem_' + i}>{item.listItem}</div>
-            ))}
-            {data.listItems.map((item,i) => (
-              <div className="marqueeList__items_item" key={'listItem_' + i}>{item.listItem}</div>
-            ))}
+
+          <div className="marqueeList__slideshow marqueeList__slideshow4">
+            <div className="marqueeList__items">
+              {data.listItems.map((item,i) => (
+                <div className="marqueeList__items_item" key={'listItem_' + i}>{item.listItem}</div>
+              ))}
+              {data.listItems.map((item,i) => (
+                <div className="marqueeList__items_item" key={'listItem_' + i}>{item.listItem}</div>
+              ))}
+            </div>
           </div>
-        </div>
+        </>}
+
 	    </section>
       {(data.background.squiggleBottom && data.background.hasBackground) && <Squiggle type={data.background.squiggleBottom} />}
     </>
