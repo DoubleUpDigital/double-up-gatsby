9464f437ad8c2e69a698b0a6f7f022642ebd5612
Justin Radomski <justin@doubleupdigital.com>
2021-07-30 18:42:20 -0400

    Logging out request

--- src/api/new-gf-entry.js
+++ src/api/new-gf-entry.js
@@ -25,7 +25,7 @@ const headers = {
 export default async function handler(req, res) {
   // Make sure we are dealing with a POST request
   if (req.method !== 'POST') {
-    console.log('Something wrong with POST request.')
+    console.log(req)
     return res.status(400).send(`This was not a POST request!`)
   }
 
